cc73403b3497257702d69cf53adfa3cf7c29dd10
goregath <goregath@github.com>
2023-08-01 11:17:08 +0200

    Update example

--- examples/demo.sh
+++ examples/demo.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
-set -eE -o pipefail
+set -euE -o pipefail
 source ustar.bash
-ustar-dump -F -o mode=0644,mtime=now a/file "lorem ipsum"
+
+pack() {
+	ustar-dump -D -o mtime=now a
+	ustar-dump -F -o mtime=now a/file "lorem ipsum"
+	ustar-dump -S -o mtime=now,target=file a/symlink
+}
+
+pack | tar -tvf -
